146fff2626db6601e8bbe6861799c66ded9554cd
Henry <hacoo36@gmail.com>
2015-06-14 16:39:20 -0700

    Added list add_item view, lists are now unique

--- lists/tests.py
+++ lists/tests.py
@@ -62,19 +62,27 @@ class ListAndItemModelTest(TestCase):
         
 class ListViewTest(TestCase):
 
-    def test_home_page_displays_all_list_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+    def test_home_page_displays_this_list_items_only(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)        
+        other_list = List.objects.create()
+        Item.objects.create(text='other item 1', list=other_list)
+        Item.objects.create(text='other item 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list/')
-        self.assertContains(response, 'itemey 1')
+        response = self.client.get('/lists/%d/' % (correct_list.id))
+
         self.assertContains(response, 'itemey 2')
+        self.assertContains(response, 'itemey 1')
+        self.assertNotContains(response, 'other item 1')
+        self.assertNotContains(response, 'other item 2')
 
-    def test_list_uses_template(self):
-        response = self.client.get('/lists/the-only-list/')
-        self.assertTemplateUsed(response, 'list.html')
-
+        
+    def test_passes_correct_list_to_templat(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id))
+        self.assertEqual(response.context['list'], correct_list)
     
 
 class NewListTest(TestCase):
@@ -94,4 +102,56 @@ class NewListTest(TestCase):
             data={'item_text': 'A new list item'}
         )
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/lists/the-only-list/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id))
+
+
+class NewItemTest(TestCase):
+
+    def can_save_multiple_POST_request_to_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id),
+            data = {'item_text': 'A new item for existing list'}
+        )
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id),
+            data = {'item_text': 'A second item for existing list'}
+        )
+
+
+        self.assertEqual(Item.objects.count, 1)
+        new_item = Item.objects.first()
+        second_item = Item.objects.second()
+        self.assertEqual(new_item.text, 'A new item for exiting list')
+        self.assertEqual(second_item.text, 'A second item for exiting list')
+        self.assertEqual(new_item.list, correct_list)
+        self.assertEqual(second_item.list, correct_list)
+
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id),
+            data = {'item_text': 'A new item for existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id))
+
+        
+
+            
+        
+
+        
+
+
+
+
+    
+    
--- superlists/urls.py
+++ superlists/urls.py
@@ -6,6 +6,9 @@ urlpatterns = patterns('',
    # url(r'^blog/', include('blog.urls')),
    # url(r'^admin/', include(admin.site.urls)),
             url(r'^$', 'lists.views.home_page', name='home'),
-            url(r'^lists/the-only-list/$', 'lists.views.view_list', name='view_list'),
-            url(r'^lists/new', 'lists.views.new_list', name='new_list')
+            url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'), # (.+) will capture anything
+            url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+            url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+            # between / / in the URL, and send it as an argument to the view.
+
 )
