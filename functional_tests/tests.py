ac2bbe32135539fc0d0320bf1624d908ddc66f99
Henry <hacoo36@gmail.com>
2015-06-15 17:11:53 -0700

    Added liveserver test option to FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,21 +4,38 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #import unittest
 import time
+import sys
 
 # A functional test story!
 # Adapted from Harry J.W. Percival "Test Driven Development with Python "
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
-    # setUp is always run at the beginning
+    #setUpClass is like the classmethod version of setUp -- it
+    #only runs once, on class initialization, rather than once for
+    #each object.
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
         
-    # tearDown is always run at the end (even if error)
+    
     def tearDown(self):
         self.browser.quit()
-
+    
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -29,7 +46,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
     def test_layout_and_styling(self):
         # Edith goes to the home page.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # It's got a beutiful ceneter input box!
@@ -56,7 +73,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         be created and retrieved. """
         # Edith is trying out a cool new online to-do list app.
         # She goes to the homepage:
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         
         # She notices the header and title mention to-do lists:
         self.assertIn('To-Do', self.browser.title)        
@@ -108,7 +125,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # A new user, Francis, goes to the site.
         ## Frances starts a new browser
         self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
